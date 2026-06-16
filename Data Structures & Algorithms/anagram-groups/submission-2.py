527e3e16074bc6dc472b9fca2aaa7f6e47cf1610
Laszlo Lazuer <visualjazz@gmail.com>
2026-06-15 21:10:38 -0600

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        answers = {}
+
+        for string in strs:
+            key = "".join(sorted(string))
+
+            if key not in answers:
+                answers[key] = []
+            answers[key].append(string)
+        return list(answers.values())
